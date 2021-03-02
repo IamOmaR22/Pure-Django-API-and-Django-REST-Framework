38e6f525fc5366aeb25f884bbea67fec6b866866
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-02 21:30:06 +0600

    Json CBV

--- updates/views.py
+++ updates/views.py
@@ -1,5 +1,9 @@
+import json  # json is a Python built in library.
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+
+from django.views.generic import View
+from pda_drf.mixins import JsonResponseMixin
 
 from .models import Update
 
@@ -8,9 +12,34 @@ from .models import Update
     # return HttpResponse(get_template().render({}))
 
 
-def update_model_detail_view(request):
+def json_example_view(request):
+    '''
+    URI (Uniform Resource Identifier) - for a REST Api
+    GET - Request
+    '''
     data = {
         'count': 1000,
         'content': 'Some new content'
     }
-    return JsonResponse(data)   # Python dictionary turns into a JSON dictionary with this shortcut.
+    json_data = json.dumps(data)
+    # return JsonResponse(data)   # Python dictionary turns into a JSON dictionary with this shortcut.
+    return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'count': 1000,
+            'content': 'Some new content'
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'count': 1000,
+            'content': 'Some new content'
+        }
+        return self.render_to_json_response(data)
+
