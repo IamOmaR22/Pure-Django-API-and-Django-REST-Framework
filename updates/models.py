37f971094f5a136d52cf6ca0716a3830f4c7fd6e
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-13 23:54:33 +0600

    serializer

--- updates/models.py
+++ updates/models.py
@@ -1,9 +1,31 @@
+import json
 from django.db import models
 from django.conf import settings
+
+from django.core.serializers import serialize
 
 
 def upload_update_image(instance, filename):   # This method is for ImageField() and used for upload_to in ImageField().
     return 'updates/{user}/{filename}'.format(user=instance.user, filename=filename)
+
+
+# Custom Query Set Manager.
+class UpdateQuerySet(models.QuerySet):
+    # def serialize(self):
+    #     qs = self
+    #     return serialize('json', qs, fields=('user', 'content', 'image'))
+
+    def serialize(self):
+        qs = self
+        final_array = []   # Empty list
+        for obj in qs:
+            stuct = json.loads(obj.serialize())
+            final_array.append(stuct)
+        return json.dumps(final_array)
+
+class UpdateManager(models.Manager):
+    def get_queryset(self):
+        return UpdateQuerySet(self.model, using=self._db)
 
 class Update(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -12,5 +34,14 @@ class Update(models.Model):
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
+    objects = UpdateManager()
+
     def __str__(self):
         return self.content or ''
+
+    def serialize(self):
+        json_data = serialize("json", [self], fields=('user', 'content', 'image'))
+        stuct = json.loads(json_data) # Opposite of dumps data. It taking json data and turned it into something we can use. loads turns it into list over dictionary [{}].
+        print(stuct)
+        data = json.dumps(stuct[0] ['fields'])  # Transform the data to being json. Trying to back in python dictionary or array to print it on what it is exactly.
+        return data
--- updates/views.py
+++ updates/views.py
@@ -1,8 +1,10 @@
 import json  # json is a Python built in library.
+
+from django.core.serializers import serialize
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-
 from django.views.generic import View
+
 from pda_drf.mixins import JsonResponseMixin
 
 from .models import Update
@@ -11,6 +13,7 @@ from .models import Update
     # return render()  # return JSON data --> JS Object Notation
     # return HttpResponse(get_template().render({}))
 
+# obj = Update.objects.get(id=1)
 
 def json_example_view(request):
     '''
@@ -43,3 +46,47 @@ class JsonCBV2(JsonResponseMixin, View):
         }
         return self.render_to_json_response(data)
 
+'''
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj, ], fields=('user', 'content'))
+        # print(data)
+        json_data = data
+        # data = {
+        #     'user': obj.user.username,
+        #     'content': obj.content
+        # }
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        # data = serialize("json", qs))  # qs is a list here. This line will serialize the data for me. All the fields are associated with the model.
+        data = serialize("json", qs, fields=('user', 'content'))  # We want the field we set. Now we can serialized the data with fields which i declared.
+        # $ python manage.py dumpdata --format json --indent 4  # You can see serialized data. Comes through by default using django's built in serializer.
+        # $ python manage.py dumpdata updates.Update --format json --indent 4  # Specific App and Model. To see this, must need to some data in model.
+        print(data)
+        json_data = data
+        # data = {
+        #     'user': obj.user.username,
+        #     'content': obj.content
+        # }
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
+'''
+
+class SerializedDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        json_data = obj.serialize()
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        json_data = Update.objects.all().serialize()  # It will give us the serialize version of query set.
+        return HttpResponse(json_data, content_type='application/json')
