243751cf69681b1fcce9edd373315e20a66ed4c8
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-30 13:42:47 +0600

    One Endpint

--- scripts/cfe_pure_api.py
+++ scripts/cfe_pure_api.py
@@ -5,8 +5,12 @@ BASE_URL = 'http://127.0.0.1:8000/'
 
 ENDPOINT = 'api/updates/'
 
-def get_list():  # ---> Lists all this out.
-    r = requests.get(BASE_URL + ENDPOINT)
+def get_list(id=None):  # ---> Lists all this out. We can give here 1,2,3,...etc or None. (id=None)
+    data = json.dumps({})
+    if id is not None:
+        data = json.dumps({'id': id})
+    r = requests.get(BASE_URL + ENDPOINT, data=data)
+    # r = requests.get(BASE_URL + ENDPOINT)
     
     print(r.status_code)
     status_code = r.status_code
@@ -16,13 +20,16 @@ def get_list():  # ---> Lists all this out.
     data = r.json()
     # print(type(data))  # It will print the list class.
     # print(type(json.dumps(data)))  # It will print the string class.
-    for obj in data:
-        # print(obj['id'])
-        if obj['id'] == 1:   # get individual id. ---> User Interaction.
-            r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
-            # print(dir(r2))
-            print(r2.json())
+    # for obj in data:
+    #     # print(obj['id'])
+    #     if obj['id'] == 1:   # get individual id. ---> User Interaction.
+    #         r2 = requests.get(BASE_URL + ENDPOINT + str(obj['id']))
+    #         # print(dir(r2))
+    #         print(r2.json())
     return data
+
+print(get_list())
+
 
 def create_update():
     new_data = {
@@ -40,7 +47,6 @@ def create_update():
         return r.json()
     return r.text
 
-# print(get_list())
 # get_list()  # Call the function
 # print(create_update())
 
@@ -48,10 +54,12 @@ def create_update():
 
 def do_obj_update():
     new_data = {
-        'content': 'Some new awesome content'
+        'id': 3,  # it can be 1,2,3,4.....etc
+        'content': 'Awesome'
     }
+    r = requests.put(BASE_URL + ENDPOINT, data=json.dumps(new_data))  # put method
     # r = requests.put(BASE_URL + ENDPOINT + '1/', data=new_data) #json.dumps(new_data))  # put method
-    r = requests.put(BASE_URL + ENDPOINT + '1/', data=json.dumps(new_data))  # put method
+    # r = requests.put(BASE_URL + ENDPOINT + '1/', data=json.dumps(new_data))  # put method
     # r = requests.put(BASE_URL + ENDPOINT + '100/', data=json.dumps(new_data))  # put method
     # new_data = {
     #     'id': 1,
@@ -71,9 +79,11 @@ def do_obj_update():
 
 def do_obj_delete():
     new_data = {
-        'content': 'Some new awesome content'
+        # 'content': 'Some new awesome content'
+        'id': 3
     }
-    r = requests.delete(BASE_URL + ENDPOINT + '1/')  # delete method
+    r = requests.delete(BASE_URL + ENDPOINT, data=json.dumps(new_data))  # delete method
+    # r = requests.delete(BASE_URL + ENDPOINT + '1/')  # delete method
     # r = requests.delete(BASE_URL + ENDPOINT + '5/')  # give 1 or 2 or 5 etc.. You can delete which one you added.
 
     # print(r.headers)
@@ -83,4 +93,4 @@ def do_obj_delete():
         return r.json()
     return r.text
 
-print(do_obj_delete())
+# print(do_obj_delete())
