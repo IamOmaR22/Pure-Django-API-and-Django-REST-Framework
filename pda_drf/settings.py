fa28ea91918faa80cf1d59e4fa4f98b0ac99a452
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-30 15:54:05 +0600

    Status app and model

--- pda_drf/settings.py
+++ pda_drf/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # Third party
+    'rest_framework',
+    # Local
     'updates',
+    'status',
 ]
 
 MIDDLEWARE = [
