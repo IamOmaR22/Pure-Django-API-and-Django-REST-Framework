d0e75f06c0aa0d0b79da630dc60c9dd18818ca5d
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-30 16:58:02 +0600

    Form Validation

--- status/models.py
+++ status/models.py
@@ -21,3 +21,7 @@ class Status(models.Model):  # fb status, instagram post, tweet, linkedin post
 
     def __str__(self):
         return str(self.content)[:50] # Upto 50 characters
+
+    class Meta:
+        verbose_name = 'Status post'
+        verbose_name_plural = 'Status posts'
