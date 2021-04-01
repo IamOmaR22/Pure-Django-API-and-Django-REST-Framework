5c921474819be7b2c7cd34306cbdd5d0b958aa40
Md. Omar Faruk <iamomar022@gmail.com>
2021-04-02 00:32:29 +0600

    Uploading and Handling Images

--- pda_drf/settings.py
+++ pda_drf/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname('pda_drf'), 'static-server', 'media-root')  # Specify the folder. I gave pda_drf
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-server', 'media-root') # Base directory
+MEDIA_URL = '/media/'
--- scripts/cfe_rest_framework_api.py
+++ scripts/cfe_rest_framework_api.py
@@ -1,6 +1,7 @@
 # pip install requests
 import requests
 
+
 ENDPOINT = 'http://127.0.0.1:8000/api/status/'
 
 def do(method='get', data={}, id=6):
@@ -38,6 +39,31 @@ def do3(method='get', data={}, is_json=True):
 
 # do3(data={'id': 8})
 # do3(method='delete', data={'id': 800})
-do3(method='delete', data={'id': 13})
+# do3(method='delete', data={'id': 13})
 # do3(method='put', data={'id': 6, 'content': 'Some new cool content', 'user': 1})
 # do3(method='post', data={'content': 'Some new cool content', 'user': 1})
+
+
+import os
+
+image_path = os.path.join(os.getcwd(), 'Screenshot_9.png')  # location of the image
+
+def do_img(method='get', data={}, is_json=True, img_path=None):
+    headers = {}
+    if is_json:
+        headers['content-type'] = 'application/json'
+        data = json.dumps(data)
+    if img_path is not None:
+        with open(image_path, 'rb') as image:
+            file_data = {
+                'image': image
+            }
+            r = requests.request(method, ENDPOINT, data=data, headers=headers, files=file_data)
+    else:
+        r = requests.request(method, ENDPOINT, data=data, headers=headers)
+    print(r.text)
+    print(r.status_code)
+    return r
+
+# do_img(method='post', data={'user': 1, 'content': ''}, is_json=False, img_path=image_path)
+do_img(method='put', data={'id': 17, 'user': 1, 'content': 'Some new biki content'}, is_json=False, img_path=image_path) # it will create another one. Will not update.
